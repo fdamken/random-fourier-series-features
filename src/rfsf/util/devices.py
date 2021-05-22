e0f529a115ab506ee03f0c79680f30b61a24040b
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-05-22 18:21:30 +0200

    Documented devices.py.

--- src/rfsf/util/devices.py
+++ src/rfsf/util/devices.py
@@ -1,9 +1,12 @@
 import torch
 
 
-def cuda():
+def cuda() -> torch.device:
+    """Gets the torch CUDA device that is currently active."""
+    assert torch.cuda.is_available(), "CUDA is not available"
     return torch.device("cuda", index=torch.cuda.current_device())
 
 
-def cpu():
+def cpu() -> torch.device:
+    """Gets the torch CPU device-"""
     return torch.device("cpu")
