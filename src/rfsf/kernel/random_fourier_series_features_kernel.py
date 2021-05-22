Fixed RFSF implementation and added amplitude normalization.

src/rfsf/kernel/random_fourier_series_features_kernel.py
@@ -25,6 +25,7 @@ class RandomFourierSeriesFeaturesKernel(DegenerateKernel):
 
         self._input_dim = input_dim
         self._num_features = num_features
+        self._num_harmonics = num_harmonics
         self._length_scale = torch.nn.Parameter(length_scale, requires_grad=length_scale.requires_grad).to(device=self.device)
         self._amplitudes_sqrt = torch.nn.Parameter(amplitudes_sqrt, requires_grad=amplitudes_sqrt.requires_grad).to(device=self.device)
         self._phases = torch.nn.Parameter(phases, requires_grad=phases.requires_grad).to(device=self.device)
@@ -38,10 +39,16 @@ class RandomFourierSeriesFeaturesKernel(DegenerateKernel):
 
     def forward_features(self, x: torch.Tensor) -> torch.Tensor:
         assert_axis_length(x, 1, self._input_dim, "x")
+
+        biases = self._biases.unsqueeze(dim=0).unsqueeze(dim=2)
+        phases = self._phases.unsqueeze(dim=0).unsqueeze(dim=1)
+
         normalization = np.sqrt(2 / self._num_features)
-        rff_activations = x @ self._weights.T / self._length_scale + self._biases
-        harmonics = (self._amplitudes * torch.cos(rff_activations.unsqueeze(dim=2) + self._phases)).sum(dim=2)
-        return normalization * harmonics
+        weighted_inputs = (x @ self._weights.T).unsqueeze(dim=2)
+        harmonic_multipliers = (torch.arange(self._num_harmonics + 1, device=self.device)).unsqueeze(dim=0).unsqueeze(dim=1)
+        harmonics_activations = harmonic_multipliers * weighted_inputs + biases + phases
+        harmonics = self._amplitudes * torch.cos(harmonics_activations)
+        return normalization * harmonics.sum(dim=2) / self._amplitudes.sum()
 
     @property
     def _amplitudes(self) -> torch.Tensor:

src/scripts/visualize_rff_kernel.py
@@ -11,25 +11,41 @@ def main():
     np.random.seed(12345)
     torch.random.manual_seed(12345)
 
-    length_scale = 1.0
-    rbf = SkLearnKernelWrapper(RBF(length_scale=length_scale))
+    length_scale = torch.tensor(1.0)
+    rbf = SkLearnKernelWrapper(RBF(length_scale=length_scale.item()))
 
-    x = torch.arange(-5.0, 5.0, 0.01).unsqueeze(dim=1)
+    x_min, x_max = -5.0, 5.0
+    x = torch.arange(x_min, x_max, 0.01).unsqueeze(dim=1)
 
     rbf_mat = rbf(x, x).numpy()
 
-    fig, axss = plt.subplots(nrows=2, ncols=7, figsize=(21, 6))
-    (ax11, ax12), *axs = axss.T
-    ax11.imshow(rbf_mat, extent=[x.min(), x.max(), x.min(), x.max()])
-    ax12.imshow(np.isclose(rbf_mat, rbf_mat), extent=[x.min(), x.max(), x.min(), x.max()], vmin=0, vmax=1)
-    ax11.set_title("Exact RBF Kernel")
-    for i, (ax21, ax22) in enumerate(axs):
-        num_features = 10 ** i
-        rff = RandomFourierFeaturesKernel(1, num_features, length_scale=torch.tensor(length_scale))
-        rff_mat = rff(x, x).numpy()
-        ax21.imshow(rff_mat, extent=[x.min(), x.max(), x.min(), x.max()])
-        ax22.imshow(np.isclose(rff_mat, rbf_mat, atol=0.01), extent=[x.min(), x.max(), x.min(), x.max()], vmin=0, vmax=1)
-        ax21.set_title(f"RFF Kernel, {num_features} Features")
+    num_num_features = 7
+    num_features_list = 10 ** np.arange(num_num_features, dtype=int)
+
+    matrices = np.empty((num_num_features + 1, len(x), len(x)))
+    matrices[0, :, :] = rbf_mat
+    for i, num_features in enumerate(num_features_list):
+        print(f"Computing RFF for {num_features=}.")
+        rfsf = RandomFourierFeaturesKernel(1, num_features, length_scale=length_scale)
+        with torch.no_grad():
+            rfsf_mat = rfsf(x, x).detach().cpu().numpy()
+        matrices[i + 1, :, :] = rfsf_mat
+    # mmax = max(np.max(matrices), np.max(rbf_mat))
+
+    figsize_multiplier = 2
+    fig, axss = plt.subplots(nrows=2, ncols=num_num_features, figsize=(figsize_multiplier * (num_num_features + 1), 1 + figsize_multiplier * 2))
+    for (ax1, ax2), (i, num_features) in zip(axss.T, enumerate(num_features_list)):
+        rff_mat = matrices[i]
+        ax1.imshow(rff_mat, extent=[x_min, x_max, x_min, x_max])
+        ax2.imshow(np.isclose(rff_mat, rbf_mat, atol=0.01), extent=[x.min(), x.max(), x.min(), x.max()], vmin=0, vmax=1)
+        ax1.set_xticks([])
+        ax1.set_yticks([])
+        ax2.set_xticks([])
+        ax2.set_yticks([])
+        if i == 0:
+            ax1.set_title("Exact RBF Kernel")
+        else:
+            ax1.set_title(f"$N = {num_features}$")
     plt.tight_layout()
     fig.show()
 