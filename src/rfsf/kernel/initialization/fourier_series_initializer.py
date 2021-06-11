Fixed computation issue in featurize method of RFSF kernel.
* Now the initial kernel is equivalent to the initialization.

src/rfsf/kernel/initialization/fourier_series_initializer.py
@@ -43,13 +43,23 @@ class FourierSeriesInitializer(ABC):
         self._amplitudes_sqrt: Optional[torch.Tensor] = None
         self._phases: Optional[torch.Tensor] = None
 
-    def __call__(self, x):
-        a = self.cosine_coefficients
-        b = self.sine_coefficients
-        n = torch.arange(1, self.num_harmonics + 1)
-        cos = np.cos(np.pi / self.half_period * n.unsqueeze(axis=0) * x.unsqueeze(axis=1))
-        sin = np.sin(np.pi / self.half_period * n.unsqueeze(axis=0) * x.unsqueeze(axis=1))
-        return a[0] / 2 + (a[1:].unsqueeze(axis=0) * cos + b[1:].unsqueeze(axis=0) * sin).sum(axis=1)
+    def __call__(self, x: torch.Tensor, *, use_sine_cosine_form: bool = True) -> torch.Tensor:
+        x = x.unsqueeze(axis=1)
+        if use_sine_cosine_form:
+            a = self.cosine_coefficients
+            b = self.sine_coefficients
+            n = torch.arange(1, self.num_harmonics + 1).unsqueeze(axis=0)
+            cos = torch.cos(np.pi / self.half_period * n * x)
+            sin = torch.sin(np.pi / self.half_period * n * x)
+            result = a[0] / 2 + (a[1:] * cos + b[1:].unsqueeze(axis=0) * sin).sum(axis=1)
+        else:
+            amplitudes = self.amplitudes_sqrt.unsqueeze(axis=0) ** 2
+            phases = self.phases.unsqueeze(axis=0)
+            n = torch.arange(0, self.num_harmonics + 1).unsqueeze(axis=0)
+            harmonics_activations = np.pi / self.half_period * n * x - phases
+            harmonics = amplitudes * torch.cos(harmonics_activations)
+            result = harmonics.sum(axis=1)
+        return result
 
     @abstractmethod
     def compute_coefficients(self) -> Tuple[torch.Tensor, torch.Tensor]:

src/rfsf/kernel/rfsf_kernel.py
@@ -35,11 +35,11 @@ class RFSFKernel(Kernel):
 
         device = self.raw_lengthscale.device
 
-        self.fourier_series_init = fourier_series_init
         self.num_samples = num_samples
         self.num_harmonics = fourier_series_init.num_harmonics
+        self.half_period = fourier_series_init.half_period
         self.amplitudes_sqrt = torch.nn.Parameter(data=fourier_series_init.amplitudes_sqrt, requires_grad=fourier_series_init.optimize_amplitudes).to(device=device)
-        self.phases = torch.nn.Parameter(data=fourier_series_init.amplitudes_sqrt, requires_grad=fourier_series_init.optimize_phases).to(device=device)
+        self.phases = torch.nn.Parameter(data=fourier_series_init.phases, requires_grad=fourier_series_init.optimize_phases).to(device=device)
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor, diag: bool = False, last_dim_is_batch: bool = False, **params) -> LazyTensor:
         """
@@ -96,14 +96,14 @@ class RFSFKernel(Kernel):
             rand_weights = torch.ones_like(rand_weights)
             rand_phases = torch.zeros_like(rand_phases)
 
-        n = torch.arange(self.num_harmonics + 1, dtype=x.dtype, device=self.raw_lengthscale.device)
+        n = torch.arange(0, self.num_harmonics + 1, dtype=x.dtype, device=self.raw_lengthscale.device)
         weighted_inputs = x.matmul(rand_weights / self.lengthscale.transpose(-1, -2)).unsqueeze(dim=-1)
-        harmonized_inputs = weighted_inputs @ n.unsqueeze(dim=0)
-        harmonics_activations = harmonized_inputs + phases + rand_phases.unsqueeze(dim=-1)
+        harmonized_inputs = np.pi / self.half_period * weighted_inputs @ n.unsqueeze(dim=0)
+        harmonics_activations = harmonized_inputs - phases + rand_phases.unsqueeze(dim=-1)
         harmonics = amplitudes * torch.cos(harmonics_activations)
 
         # TODO: Is it helpful to normalize over the amplitudes?
-        return 2 * harmonics.sum(dim=-1)
+        return harmonics.sum(dim=-1)
 
     @property
     def _amplitudes(self) -> torch.Tensor:

src/tests/kernel/initialization/test_fourier_series_initializer.py
@@ -26,7 +26,10 @@ def test_non_negative_sine_coefficients():
 # noinspection PyTypeChecker
 def test_non_negative_sine_call():
     init = NonNegativeSineInitializer()
-    x = torch.arange(-10, 10, 0.01)
+    x = torch.arange(-10, 10, 0.1)
     expected = 9 * torch.sin(x) + 2
-    actual = init(x)
-    assert torch.allclose(actual, expected)
+    actual_sine_cosine = init(x, use_sine_cosine_form=True)
+    actual_amplitude_phase = init(x, use_sine_cosine_form=False)
+    assert torch.allclose(actual_sine_cosine, expected)
+    assert torch.allclose(actual_amplitude_phase, expected, atol=1e-5)
+    assert torch.allclose(actual_amplitude_phase, actual_sine_cosine, atol=1e-5)

src/tests/kernel/test_random_fourier_series_features_kernel.py
@@ -3,11 +3,26 @@ import pytest
 import torch
 
 from rfsf.kernel.initialization.random_fourier_series_initializer import RandomFourierSeriesInitializer
+from rfsf.kernel.initialization.relu_fourier_series_initializer import ReLUFourierSeriesInitializer
 from rfsf.kernel.rfsf_kernel import RFSFKernel
 
 
 def test_constructor():
     RFSFKernel(100, RandomFourierSeriesInitializer(10, 0))
+
+
+def test_initialization():
+    num_samples = 1
+    half_period = 1
+    init = ReLUFourierSeriesInitializer(8, half_period)
+    rfsf = RFSFKernel(num_samples, init)
+    rfsf.lengthscale = torch.tensor(1.0)
+    x = torch.arange(-3 * half_period, 3 * half_period, 0.01)
+    rfsf._init_weights_and_phases(1, num_samples)
+    actual = rfsf._featurize(x.unsqueeze(-1), identity_randoms=True)[:, 0]
+    expected = init(x, use_sine_cosine_form=False)
+    assert actual.shape == expected.shape
+    assert torch.allclose(actual, expected, atol=1e-5)
 
 
 @pytest.mark.parametrize("batch_a", [1, 3])
@@ -16,7 +31,7 @@ def test_constructor():
 @pytest.mark.parametrize("samples_q", [1, 2, 3, 5])
 @pytest.mark.parametrize("dim", [1, 2, 3, 5])
 def test_compute(batch_a, batch_b, samples_p, samples_q, dim):
-    rfsf = RFSFKernel(100, RandomFourierSeriesInitializer(10, 0))
+    rfsf = RFSFKernel(100, RandomFourierSeriesInitializer(10, 1))
     p = torch.rand(size=(batch_a, batch_b, samples_p, dim))
     q = torch.rand(size=(batch_a, batch_b, samples_q, dim))
     with gpytorch.settings.lazily_evaluate_kernels(False):
@@ -29,7 +44,7 @@ def test_compute(batch_a, batch_b, samples_p, samples_q, dim):
 @pytest.mark.parametrize("samples", [1, 2, 3, 5])
 @pytest.mark.parametrize("dim", [1, 2, 3, 5])
 def test_compute_diag(batch_a, batch_b, samples, dim):
-    rfsf = RFSFKernel(100, RandomFourierSeriesInitializer(10, 0))
+    rfsf = RFSFKernel(100, RandomFourierSeriesInitializer(10, 1))
     p = torch.rand(size=(batch_a, batch_b, samples, dim))
     q = torch.rand(size=(batch_a, batch_b, samples, dim))
     with gpytorch.settings.lazily_evaluate_kernels(False):
@@ -43,7 +58,7 @@ def test_compute_diag(batch_a, batch_b, samples, dim):
 @pytest.mark.parametrize("dim", [1, 2, 3, 5])
 @pytest.mark.parametrize("num_samples", [1, 2, 3, 5])
 def test_compute_eq(batch_a, batch_b, samples, dim, num_samples):
-    rfsf = RFSFKernel(num_samples, RandomFourierSeriesInitializer(10, 0))
+    rfsf = RFSFKernel(num_samples, RandomFourierSeriesInitializer(10, 1))
     p = torch.rand(size=(batch_a, batch_b, samples, dim))
     with gpytorch.settings.lazily_evaluate_kernels(False):
         cov = rfsf(p)