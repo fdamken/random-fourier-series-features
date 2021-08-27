4c8c4802e065467e33bfdec9c39c1367f9085b37
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-08-27 17:11:47 +0200

    Added script for evaluating multiple datasets and train/test split seeds.

---
+++ src/scripts/bash/multi_datasets_and_seeds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ ! -d src ]]; then
+    echo "E: Not executing from the repository root." >&2
+    exit 1
+fi
+
+datasets=$1
+shift
+num_eval=$1
+shift
+
+echo "Activating virtualenv."
+source venv/bin/activate
+
+echo "Starting training."
+for dataset in $datasets; do
+    for train_test_split_seed in $(seq 1 $num_eval); do
+        echo "Evaluating dataset $dataset, split seed $train_test_split_seed"
+        PYTHONPATH=src python src/experiments/experiment.py with dataset.name=$dataset dataset.train_test_split_seed=$train_test_split_seed $*
+    done
+done
