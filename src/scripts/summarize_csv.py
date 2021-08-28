3997a44393690d9240dcbbcea009a8a4b93f074c
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-08-28 10:02:54 +0200

    Added some results for the RFSF kernel.

---
+++ src/scripts/summarize_csv.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+import pandas as pd
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("file")
+    args = parser.parse_args()
+    file = args.file
+
+    df = pd.read_csv(file)
+    print(df.describe())
+
+
+if __name__ == '__main__':
+    main()
