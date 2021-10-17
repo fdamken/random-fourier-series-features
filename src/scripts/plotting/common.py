65fb31f21fb7d022e36982b496b676c546e485ce
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2021-10-17 16:18:48 +0200

    Fix GP plot with epistemic/aleatoric uncertainty.

--- src/scripts/plotting/common.py
+++ src/scripts/plotting/common.py
@@ -71,20 +71,28 @@ def plot_process(
     model.eval()
     with torch.no_grad(), gpytorch.settings.fast_pred_var():
         test_x_transformed = pre_processor.transform_inputs(test_x)
-        pred = model.likelihood(model(test_x_transformed))
+        posterior = model(test_x_transformed)
+        likelihood = model.likelihood(posterior)
+    mean = pre_processor.inverse_transform_targets(likelihood.mean)
+    epistemic_transformed_uncertainty = posterior.variance
+    aleatoric_transformed_uncertainty = likelihood.variance - posterior.variance
+
+    epistemic_confidence = 2 * pre_processor.inverse_transform_target_std_devs(torch.sqrt(epistemic_transformed_uncertainty))
+    combined_confidence = 2 * pre_processor.inverse_transform_target_std_devs(torch.sqrt(epistemic_transformed_uncertainty + aleatoric_transformed_uncertainty))
+    lower_epistemic, upper_epistemic = mean - epistemic_confidence, mean + epistemic_confidence
+    lower_combined, upper_combined = mean - combined_confidence, mean + combined_confidence
+
     fig, ax = plt.subplots() if fig_ax is None else fig_ax
-    pred_mean = pre_processor.inverse_transform_targets(pred.mean)
-    # TODO: Is this inverse transformation correct? It does not seem so, but the plots look good…
-    pred_conf = torch.tensor(2) * pre_processor.inverse_transform_target_std_devs(pred.stddev)
-    lower, upper = pred_mean - pred_conf, pred_mean + pred_conf
     # Make one-dimensional for plotting.
     train_x, test_x = train_x.squeeze(), test_x.squeeze()
     ax.scatter(to_numpy(train_x), to_numpy(train_y), color="black", marker="*", s=100, label="Observed Data", zorder=3)
     ax.plot(to_numpy(test_x), to_numpy(test_y), color="black", label="True Func.", zorder=0)
     for _, c in zip(range(num_samples), sample_color_cycler):
-        ax.plot(to_numpy(test_x), to_numpy(pre_processor.inverse_transform_targets(pred.sample())), color=c["color"], alpha=0.3, zorder=2)
-    ax.plot(to_numpy(test_x), to_numpy(pred_mean), color="tab:blue", label="Mean", zorder=3)
-    ax.fill_between(to_numpy(test_x), to_numpy(lower), to_numpy(upper), color="tab:blue", alpha=0.2, label=r"Confidence", zorder=1)
+        ax.plot(to_numpy(test_x), to_numpy(pre_processor.inverse_transform_targets(likelihood.sample())), color=c["color"], alpha=0.3, zorder=3)
+    ax.plot(to_numpy(test_x), to_numpy(mean), color="tab:blue", label="Mean", zorder=4)
+    ax.fill_between(to_numpy(test_x), to_numpy(lower_epistemic), to_numpy(upper_epistemic), color="tab:blue", alpha=0.2, label=r"Epistemic", zorder=1)
+    ax.fill_between(to_numpy(test_x), to_numpy(lower_epistemic), to_numpy(lower_combined), color="tab:orange", alpha=0.2, label="Aleatoric", zorder=2)
+    ax.fill_between(to_numpy(test_x), to_numpy(upper_epistemic), to_numpy(upper_combined), color="tab:orange", alpha=0.2, zorder=2)
     if y_lim is not None:
         ax.set_ylim(y_lim)
     ax.set_title(title + title_suffix)
