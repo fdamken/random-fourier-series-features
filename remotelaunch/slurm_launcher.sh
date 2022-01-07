bc34ae1f4054c7b00c4f6d115edad53b2c02a794
Fabian Damken <fabian.damken@stud.tu-darmstadt.de>
2022-01-07 15:27:47 +0100

    Try to fix SLURM script.

--- remotelaunch/slurm_launcher.sh
+++ remotelaunch/slurm_launcher.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-
-###############################################################################
 #SBATCH --job-name run_all_seeds
 #SBATCH --array 0-19
-#SBATCH --time 06:00:00
+#SBATCH --time 06:00
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 4
-#SBATCH --mem-per-cpu=4096
-#SBATCH --gres=gpu
+#SBATCH --mem-per-cpu 4096
+#SBATCH --gres gpu
 #SBATCH -o /home/fd15hava/logs/%A_%a-out.txt
 #SBATCH -e /home/fd15hava/logs/%A_%a-err.txt
-###############################################################################
+
+set -o errexit
+set -o nounset
 
 echo "Starting Job $SLURM_JOB_ID; Array Job $SLURM_ARRAY_JOB_ID Index $SLURM_ARRAY_TASK_ID."
 
